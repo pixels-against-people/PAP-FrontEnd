b8926fda682c186dda5d915b2210f13c841f81ac
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-02-27 16:47:16 -0800

    everything sort of works

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@
 /* eslint-disable semi */
 import React , { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Home from './home'
+import Home from './pages/home'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Home />
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable semi */
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { state: 'state'}
+  }
+
+  render() {
+    return (
+      <p>big yeet</p>
+    )
+  }
+}
+
+export default Home
