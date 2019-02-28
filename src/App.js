5e64b7c2baf57a34560d2bacbf9f34f299f48720
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-02-27 16:18:29 -0800

    is broke

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router'
-import Home from './home'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/home'
 import './App.css';
 
 class App extends Component {
