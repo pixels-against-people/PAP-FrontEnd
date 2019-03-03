93fcaf757f6dfc96b7251efdef1dff6f7cb6d06c
Sean <Spglancy@gmail.com>
2019-03-03 13:42:22 -0800

    finished basic styling for new game page

--- src/pages/NewGame.js
+++ src/pages/NewGame.js
@@ -14,6 +14,7 @@ class NewGame extends Component {
   }
 
   generateSets() {
+    // generates setlist from the CAH-API
     fetch('https://cards-against-humanity-api.herokuapp.com/sets')
       .then((sets) => {
         sets.map(({ setName }) => {
@@ -27,6 +28,7 @@ class NewGame extends Component {
   }
 
   highlightSet(setName) {
+    // adds selected set to state if not already included, removes if it is
     const selected = this.state.selectedSets
     if (selected.includes(setName)) {
       selected.pop(selected.indexOf(setName))
@@ -40,21 +42,28 @@ class NewGame extends Component {
   render() {
     return (
       <div className="newGameContainer">
+        <div className="playersContainer">
+          <h1>Current Players</h1>
+          <div className="player">
+            <img align="middle" src="http://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg" alt="placeholder" />
+            <span>Jack</span>
+          </div>
+        </div>
         <div className="setContainer">
           <h1>Select The Decks You'd Like to Use</h1>
-          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="highlighted" />
-          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="highlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="unhighlighted" />
+          <SetSelect onClick={() => this.highlightSet('pack1')} setName="pack1" highlight="unhighlighted" />
           <SetSelect onClick={() => this.highlightSet('pack2')} setName="pack2" highlight="highlighted" />
           <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="highlighted" />
           <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="highlighted" />
@@ -63,7 +72,9 @@ class NewGame extends Component {
           <SetSelect onClick={() => this.highlightSet('pack3')} setName="pack3" highlight="highlighted" />
           <SetSelect onClick={() => this.highlightSet('pack4')} setName="pack4" highlight="highlighted" />
         </div>
-        <Link to="play-game">Start!</Link>
+        <div className="startButton">
+          <Link className="Link" to="play-game">Start!</Link>
+        </div>
       </div>
     )
   }
