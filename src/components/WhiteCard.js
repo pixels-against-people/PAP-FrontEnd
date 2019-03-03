08758a283d0bef71994e72cafe56e4a3b57d15c7
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-03-03 12:24:08 -0800

    fixed linting errors

--- src/components/WhiteCard.js
+++ src/components/WhiteCard.js
@@ -1,18 +1,26 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable semi */
 import React, { Component } from 'react'
 import './WhiteCard.css'
 
 class WhiteCard extends Component {
-
-
-  render(){
+  render() {
+    // eslint-disable-next-line react/prop-types
     const { text, selected, onclick, submit } = this.props
-    
-    return(
-      <div onClick={onclick} className={"white-card " + (selected? 'selected' : '')}>
-          <p>{text}</p>
-          {selected && 
-          <div className="overlay" onClick={submit}> <p>Confirm?</p> </div>
-          }
+
+    return (
+      <div onClick={onclick} className={`white-card ${selected ? 'selected' : ''}`}>
+        <p>{text}</p>
+        {selected && (
+        <div className="overlay" onClick={submit}>
+          <p>Confirm?</p>
+        </div>
+        )
+        }
       </div>
     )
   }
--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -1,14 +1,16 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable semi */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react'
 import './GameScreen.css'
 import WhiteCard from '../components/WhiteCard'
-class GameScreen extends Component {
 
+class GameScreen extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      blackCard: {text: 'Why can\'t I sleep at night' , pick: 1},
+      blackCard: { text: 'Why can\'t I sleep at night', pick: 1 },
       whiteCards: ['Coat hanger abortions.', 'Man meat.', 'Autocannibalism.', 'Praying the gay away.', 'Same-sex ice dancing.', 'Ethnic cleansing.', 'Battlefield amputations.', 'An uppercut.', 'Shiny objects.'],
       players: ['papas home', 'urmom', 'WOAH.css'],
       selectedWhite: null,
@@ -17,40 +19,58 @@ class GameScreen extends Component {
   }
 
   selectCard(card) {
-    this.setState({selectedWhite:card})
+    this.setState({ selectedWhite: card })
   }
 
   submit(card) {
+    // eslint-disable-next-line prefer-const
     let { whiteCards, playedCards } = this.state;
-    whiteCards.splice(whiteCards.indexOf(card),1)
+    whiteCards.splice(whiteCards.indexOf(card), 1)
     playedCards = playedCards.concat(card)
-    this.setState({whiteCards, playedCards})
+    this.setState({ whiteCards, playedCards })
   }
 
   render() {
-    return(
+    const {
+      players,
+      playedCards,
+      blackCard,
+      whiteCards,
+      selectedWhite,
+    } = this.state
+    return (
+
       <div className="game-screen">
         <div className="players">
           <h1>Players</h1>
           <ul>
-            {this.state.players.map(player => {
-              return(
-                <li className='player' key={player}>{player}</li>
+            // eslint-disable-next-line react/destructuring-assignment
+            {players.map((player) => {
+              return (
+
+                <li className="player" key={player}>{player}</li>
               )
             })}
           </ul>
         </div>
-          <div className="play-area">
-            <div className="black-card">{this.state.blackCard.text}</div>
-            {this.state.playedCards.map(card => <p key={card}>{card}</p>)}
-            
-          </div>
+        <div className="play-area">
+          <div className="black-card">{blackCard.text}</div>
+          {playedCards.map(card => <p key={card}>{card}</p>)}
+        </div>
         <div className="chat"><h1>Chat</h1></div>
         <div className="white-cards">
           <ul>
-            {this.state.whiteCards.map(card => {
-              return(
-                <li key={card}><WhiteCard text={card} selected={card === this.state.selectedWhite} onclick={e => this.selectCard(card)} submit={e => this.submit(card)} /></li>
+            {whiteCards.map((card) => {
+              return (
+
+                <li key={card}>
+                  <WhiteCard
+                    text={card}
+                    selected={card === selectedWhite}
+                    onclick={() => this.selectCard(card)}
+                    submit={() => this.submit(card)}
+                  />
+                </li>
               )
             })}
           </ul>
