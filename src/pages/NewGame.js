f51c40fcc6f0ad89110d079c0160eac8718dfc3e
Sean <Spglancy@gmail.com>
2019-03-02 22:36:52 -0800

    wrote populating function for cardsets

--- src/pages/NewGame.js
+++ src/pages/NewGame.js
@@ -1,9 +1,53 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable semi */
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-class NewGame extends Component {
+import './NewGame.css'
+import SetSelect from '../components/SetSelect'
 
-  render(){
-    return (<Link to="play-game" >Start!</Link>)
+class NewGame extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedSets: []
+    }
+  }
+
+  generateSets() {
+    fetch('https://cards-against-humanity-api.herokuapp.com/sets')
+      .then((sets) => {
+        sets.map(({ setName }) => {
+          if (this.state.selectedSets.includes(setName)) {
+            // eslint-disable-next-line react/jsx-boolean-value
+            return <SetSelect setName={setName} highlight="highlighted" />
+          }
+          return <SetSelect setName={setName} highlight="" />
+        })
+      })
+  }
+
+  highlightSet(setName) {
+    const selected = this.state.selectedSets
+    if (selected.includes(setName)) {
+      selected.pop(selected.indexOf(setName))
+    } else {
+      selected.append(setName)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <SetSelect onClick={this.highlightSet('pack1')} setName="pack1" highlighted={true}/>
+          <SetSelect onClick={this.highlightSet('pack2')} setName="pack2" />
+          <SetSelect onClick={this.highlightSet('pack3')} setName="pack3" />
+          <SetSelect onClick={this.highlightSet('pack4')} setName="pack4" />
+          <p>is work?</p>
+        </div>
+        <Link to="play-game">Start!</Link>
+      </div>
+    )
   }
 }
 
