d0b58164bc4782b7ee8e5bd1de2489d7f1bc4f5e
Sean <Spglancy@gmail.com>
2019-03-05 10:57:56 -0800

    setup up deck fetch on homepage

---
+++ src/components/deckPreview.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+import React from 'react'
+
+function deckPreview(props) {
+  const { deck } = props
+  return (
+    <div>
+      <h1>{deck}</h1>
+    </div>
+  )
+}
+
+export default deckPreview
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,12 +5,25 @@ import React, { Component } from 'react'
 class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = { state: 'state'}
+    this.state = {
+      decks: [],
+    }
+  }
+
+  componentDidMount() {
+    // generates setlist from the CAH-API
+    fetch('https://cards-against-humanity-api.herokuapp.com/sets')
+      .then(response => response.json())
+      .then((sets) => {
+        this.setState({ decks: sets })
+      }).catch(err => console.log(err.message))
   }
 
   render() {
     return (
-      <p>big yeet</p>
+      <div className="homeContainer">
+
+      </div>
     )
   }
 }
